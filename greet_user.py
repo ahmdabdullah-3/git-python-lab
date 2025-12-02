18dac5cb5f6098ba611d7dad9f4825fdd162e6f9
Your Name <your@email.com>
2025-12-02 11:54:26 +0500

    Add three Python function files

---
+++ greet_user.py
@@ -0,0 +1,6 @@
+def greet(name):
+    print(f"Good morning, {name}!")
+
+
+name = input("What's your name? ")
+greet(name)
---
+++ multiply.py
@@ -0,0 +1,11 @@
+def multiply(a, b):
+    product = a * b
+    return product
+
+
+print("Enter two numbers:")
+n1 = int(input())
+n2 = int(input())
+
+result = multiply(n1, n2)
+print(f"Product: {result}")
